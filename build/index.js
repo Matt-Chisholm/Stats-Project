72e0f488e853683ddfefec99d566ae79ceba616a
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-08-07 08:54:33 -0700

    learning about enums

--- build/index.js
+++ build/index.js
@@ -9,4 +9,21 @@ const matches = fs_1.default.readFileSync('football.csv', {
 }).split('\n').map((row) => {
     return row.split(',');
 });
-console.log(matches);
+// enum - enumeration
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+;
+let manUnitedWins = 0;
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games.`);

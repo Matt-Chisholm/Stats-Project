f9cefdda06c9ecd4edb55687eb1e50572b844127
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-08-10 11:11:15 -0700

    learning about generics

--- src/CSVFileReader.ts
+++ src/CSVFileReader.ts
@@ -4,11 +4,14 @@ import { MatchResult } from './MatchResult';
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-export class CsvFileReader {
-    data: MatchData[] = [];
+export abstract class CsvFileReader<T> {
+    data: T[] = [];
 
     constructor(public filename: string) {
     }
+
+
+    abstract mapRow(row: string[]): T;
 
     read(): void {
         this.data = fs.readFileSync(this.filename, {
@@ -19,17 +22,5 @@ export class CsvFileReader {
                 return row.split(',');
             })
             .map(this.mapRow);
-    }
-
-    mapRow(row: string[]): MatchData {
-        return [
-            dateStringToDate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5] as MatchResult,
-            row[6]
-        ];
     }
 }

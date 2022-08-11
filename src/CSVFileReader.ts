179a52b6cd46f1c8751f19a8ee5ae46371d2b1bc
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-08-11 10:52:24 -0700

    using matchdata with a generic class

--- src/CSVFileReader.ts
+++ src/CSVFileReader.ts
@@ -1,8 +1,5 @@
 import fs from 'fs';
-import { dateStringToDate } from './utils';
-import { MatchResult } from './MatchResult';
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
 
 export abstract class CsvFileReader<T> {
     data: T[] = [];
